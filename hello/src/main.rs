d67837fef5cc24a998d17de9879d470a533dace3
Lidia <80785509+LidiaOrfanu@users.noreply.github.com>
2023-06-30 08:52:31 +0200

    helloWorld and intro to strings in Rust

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,12 +1,10 @@
-mod models;
-
 fn main() {
     let var = "four";
     if var.len() == 4 {
         println!("Hello, world!");
     }
-    let var2 = String::from("vlad");
-    if var2.chars().nth(1).expect("I expect an error") == 'l' {
+    let var2 = String::from("four");
+    if var2.chars().nth(1).expect("I expect an error") == 'o' {
         println!("Hello {}", var2);
     }
     let mut num = 0;
