cef62a82cfea3c6ed9c50f46473888f92006a4f0
Lidia <80785509+LidiaOrfanu@users.noreply.github.com>
2023-06-30 15:59:52 +0200

    first try fizzbuzz

---
+++ fizzbuzz/src/main.rs
@@ -0,0 +1,16 @@
+fn fizzbuzz(num: u8) {
+    for i in 1..=num {
+        if i % 3 == 0 && i % 5 == 0 {
+            println!("FizzBuzz");
+        } else if i % 3 == 0 {
+            println!("Fizz");
+        } else if i % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{i}");
+        }
+    }
+}
+fn main() {
+    fizzbuzz(20);
+}
