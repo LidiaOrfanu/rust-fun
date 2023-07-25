b31c4bd38513d36bc972469b34bc53565ab18cda
Lidia <80785509+LidiaOrfanu@users.noreply.github.com>
2023-07-25 11:27:49 +0200

    small correction fizz, add new proj

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -4,13 +4,13 @@ use std::io;
 use fizzbuzz::fizzbuzzfolder::fizzbuzzfile;
 
 fn main() {
-    println!("{}", "Please provide a number: 0 - 255".yellow());
+    println!("{}", "Please provide a number: 1 - 100".yellow());
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
     let input: u8 = match input.trim().parse() {
         Ok(num) => num,
         Err(_) => {
-            println!("{}", "Only positive numbers: 0 - 255!".red());
+            println!("{}", "Only positive numbers: 1 - 100!".red());
             return;
         }
     };
---
+++ oldfizzbuzz/src/main.rs
@@ -0,0 +1,27 @@
+use colored::Colorize;
+use std::io;
+
+fn fizzbuzz(num: u8) {
+    for i in 1..=num {
+        match (i % 3 == 0, i % 5 == 0) {
+            (true, true) => println!("FizzBuzz"),
+            (true, false) => println!("Fizz"),
+            (false, true) => println!("Buzz"),
+            (false, false) => println!("{i}"),
+        };
+    }
+}
+
+fn main() {
+    println!("{}", "Please provide a number: 0 - 255".yellow());
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let input: u8 = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("{}", "Only positive numbers: 0 - 255!".red());
+            return;
+        }
+    };
+    fizzbuzz(input);
+}
